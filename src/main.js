871a1136fe78cbb26d3fcbf7b6ab428ba985e897
gaoxizhong <296203386@qq.com>
2023-07-18 18:13:20 +0800

    1

--- src/main.js
+++ src/main.js
@@ -2,5 +2,20 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+// baseUrl = '/api
+import axios from '@/axios/index';
+import '@/styles/index.scss'; // global css
+
+
+Vue.prototype.$axios = axios;
+// set ElementUI lang to EN
+Vue.use(ElementUI, { locale });
+// 如果想要中文版 element-ui，按如下方式声明
+// Vue.use(ElementUI)
+
+
 
 createApp(App).use(store).use(router).mount("#app");
