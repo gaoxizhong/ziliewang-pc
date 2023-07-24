7e9bfd3f3ce97f705b8b5b3829db360af55aee9c
gaohaiyong <296203386@qq.com>
2023-07-25 00:39:08 +0800

    开发我的简历

--- src/views/talentSide/components/myResume/myInfo.vue
+++ src/views/talentSide/components/myResume/myInfo.vue
@@ -6,7 +6,7 @@
         <div style="flex: 1;padding-left: 0.9rem;">
           <div class="info-1">
             <span class="info-name">{{ data.name }}</span>
-            <span class="info-set" @click="clickRedactBtn( 'redact_info' )">编辑</span>
+            <span class="info-set" @click="clickRedactBtn(1)">编辑</span>
           </div>
           <ul class="info-2">
             <li>
@@ -40,13 +40,59 @@
           </ul>
         </div>
       </div>
-      <div class="redact-title-top" v-else>12131231231</div>
-      <div class="info-title-bottom">
+      <div class="redact-title-top" v-else>
+
+        <div class="mb20 redact-item">
+          <div class="item-label">姓名</div>
+          <div class="item-content">
+            <el-input v-model="infoData.name" placeholder="请输入姓名" clearable></el-input>
+          </div>
+        </div>
+
+        <div class="mb20 redact-item">
+          <div class="item-label">邮箱</div>
+          <div class="item-content">
+            <el-input v-model="infoData.email" placeholder="请输入邮箱" clearable></el-input>
+          </div>
+        </div>
+
+        <div class="mb20 redact-item">
+          <div class="item-label">性别</div>
+          <div class="item-content">
+            <div class="radio-item" :class="radio == 1 ? 'radio-checked':'' " @click="clickRadio(1)">男</div>
+            <div class="radio-item" :class="radio == 2 ? 'radio-checked':'' " @click="clickRadio(2)">女</div>
+          </div>
+        </div>
+        
+        <div class="form-btns">
+          <el-button @click="clickInfoCancelBtn">取消</el-button>
+          <el-button type="primary" @click="clickInfoVerifyBtn">确定</el-button>
+        </div>
+        
+      </div>
+      <div class="info-title-bottom" v-if="!redact_spot">
         <div class="jobExpectation-title-box">
           <span class="info-t">优势亮点</span>
-          <span class="info-set">编辑</span>
+          <span class="info-set" @click="clickRedactBtn(2)">编辑</span>
         </div>
-        <div class="info-title-x">{{ data.advantages_highlights }}</div>
+        <div class="info-title-x">{{ data.advantages_highlights?data.advantages_highlights:'暂无' }}</div>
+      </div>
+      <div class="redact-title-bottom" v-else>
+        <div class="jobExpectation-title-box">
+          <span class="info-t">优势亮点</span>
+        </div>
+        <div class="textarea-box">
+          <el-input
+            type="textarea"
+            :rows="4"
+            placeholder="请输入内容"
+            v-model="infoData.advantages_highlights">
+          </el-input>
+        </div>
+        <div class="form-spotbtns">
+          <el-button @click="clickSpotCancelBtn">取消</el-button>
+          <el-button type="primary" @click="clickSpotVerifyBtn">确定</el-button>
+        </div>
       </div>
     </div>
   </div>
@@ -69,21 +115,53 @@ export default {
   },
   data(){
     return{
-      redact_info: false,
-      redact_brightSpot: false,
+      redact_info: false, 
+      redact_spot: false,
+      infoData: {},
+      radio: 0,
     }
   },
   mounted(){
-    console.log(this.data)
-    this.data = this.data;
+  
   },
   computed: {
     
   },
   methods: {
+    clickRadio(n){
+      this.infoData.radio = n;
+      this.radio = n;
+      console.log(this.infoData)
+    },
     clickRedactBtn(n){
-      this.redact_info = true;
-    },  
+      this.infoData = JSON.parse(JSON.stringify(this.data));
+      this.radio = this.infoData.radio;
+      if(n == 1){
+        this.redact_info = true;
+      }else if(n == 2){
+        this.redact_spot = true;
+      }
+      
+    }, 
+    // 点击编辑个人信息取消按钮
+    clickInfoCancelBtn(){
+      this.redact_info = false;
+    },
+    // 点击编辑个人信息确定按钮
+    clickInfoVerifyBtn(){
+
+      this.redact_info = false;
+    },
+    // 点击优势亮点取消按钮
+    clickSpotCancelBtn(){
+      this.redact_spot = false;
+    },
+    // 点击优势亮点确定按钮
+    clickSpotVerifyBtn(){
+
+      this.redact_spot = false;
+    },
+
   },
 };
 </script>
@@ -93,7 +171,6 @@ export default {
     width: 100%;
     border-radius: 6px;
     background: #fff;
-    padding: 24px 30px;
     margin-bottom: 16px;
     .jobExpectation-title-box{
       display: flex;
@@ -110,12 +187,15 @@ export default {
         height: 20px;
         cursor: pointer;
       }
+
     }
   }
   .info-title-box{
     .info-title-top{
       width: 100%;
       display: flex;
+      padding: 24px 30px;
+
       img{
         width: 64px;
         height: 64px;
@@ -158,8 +238,69 @@ export default {
         }
       }
     }
+    .redact-title-top{
+      padding: 24px 15px;
+      background: #F4F5F7;
+      text-align: left;
+      position: relative;
+      .redact-item{
+        display: inline-block;
+        width: 50%;
+        vertical-align: top;
+        padding: 0 15px;
+        .item-label{
+          font-size: 14px;
+          text-align: right;
+          vertical-align: middle;
+          color: #495060;
+          line-height: 1;
+          box-sizing: border-box;
+          float: none;
+          display: inline-block;
+          padding: 0 0 10px;
+        }
+        .item-content {
+          position: relative;
+          line-height: 32px;
+          display: flex;
+          .radio-item{
+            flex: 1;
+            min-width: auto;
+            line-height: 36px;
+            display: inline-block;
+            border: 1px solid #e3e7ed;
+            background-color: #fff;
+            text-align: center;
+            cursor: pointer;
+            color: #9fa3b0;
+            margin-right: 20px;
+            white-space: nowrap;
+            position: relative;
+            vertical-align: middle;
+            &:last-of-type {
+              margin-right: 0;
+            }
+          }
+          .radio-checked {
+            border-color: $g_color;
+            color: $g_color;
+            background-color: #effbfa;
+          }
+        }
+      }
+      .redact-item:nth-child(odd) {
+        margin-bottom: 0;
+      }
+      .form-btns{
+        position: absolute;
+        bottom: 26px;
+        right: 15px;
+        text-align: right;
+        padding: 0 15px;
+      }
+    }
     .info-title-bottom{
-      margin-top: 38px;
+      padding: 24px 30px;
       .info-title-x{
         padding: 10px 0;
         margin-top: 6px;
@@ -170,11 +311,43 @@ export default {
         text-align: left;
       }
     }
+    .redact-title-bottom{
+      padding: 24px 30px;
+      background: #F4F5F7;
+      text-align: left;
+      position: relative;
+      .textarea-box{
+        width: 100%;
+        margin-top: 10px; 
+        font-size: 14px;
+        /deep/ .el-textarea__inner{
+          font-size: 14px;
+          &:focus{
+            border-color: $g_color;
+          }
+        }
+      }
+      .form-spotbtns{
+        margin-top: 16px;
+        width: 100%;
+        text-align: right;
+      }
+    }
+    /deep/ .el-button{
+      padding: 10px 30px;
+    }
+    /deep/ .el-button--primary{
+      background-color: $g_color;
+      border-color: $g_color;
+    }
   }
   .info-set{
-    font-size: 0.7rem;
+    font-size: 14px;
     font-weight: 400;
     color: $g_color;
     cursor: pointer;
+  }
+  .mb20{
+    margin-bottom: 20px;
   }
 </style>
--- src/views/talentSide/myResume/index.vue
+++ src/views/talentSide/myResume/index.vue
@@ -4,7 +4,7 @@
     <div class="tab-box">
       <div :class="is_titleTab == 1? 'hover':'' " @click="clickTitleTab(1)">预览简历</div>
       <div :class="is_titleTab == 2? 'hover':'' " @click="clickTitleTab(2)">刷新简历</div>
-      <div :class="is_titleTab == 3? 'hover':'' " @click="clickTitleTab(3)">建立优化</div>
+      <div :class="is_titleTab == 3? 'hover':'' " @click="clickTitleTab(3)">简历优化</div>
     </div>
     
     <div class="info-box">
@@ -13,9 +13,9 @@
           <MyInfo v-if="infoData.basic_info" :data="infoData.basic_info" />  
         <!-- 个人信息 结束 -->
 
-        <!-- 优势亮点 开始 -->
+        <!-- 求职期望 开始 -->
         <div class="jobExpectation-box myResume-plate">
-          <div class="jobExpectation-title-box">
+          <div class="myResume-plate-title-box">
             <span class="info-t">求职期望</span>
            <img src="../../../assets/image/Frame_7.png" alt="" class="info-icon-img"/>
           </div>
@@ -31,21 +31,170 @@
                     <span>|</span>
                     <span>全部行业</span>
                   </span>
-                 
                 </div>
-                <div class="info-set">设置</div>
+                <div class="info-set">
+                  <span>删除</span>
+                  <span>/</span>
+                  <span>编辑</span>
+                </div>
               </li>
-              <li></li>
+              <li>
+                <div class="li-name">
+                  <span class="li-name-1">首席执行官CEO/总裁/总经理</span>
+                  <span class="li-name-2">
+                    <span>8k - 13k x 12薪</span>
+                    <span>|</span>
+                    <span> 宁波</span>
+                    <span>|</span>
+                    <span>全部行业</span>
+                  </span>
+                </div>
+                <div class="info-set">
+                  <span>删除</span>
+                  <span>/</span>
+                  <span>编辑</span>
+                </div>
+              </li>
             </ul>
 
           </div>
         </div>
-        <!-- 优势亮点 结束 -->
+        <!-- 求职期望 结束 -->
+
+        <!-- 工作经历 开始 -->
+        <div class="workExperience-box myResume-plate">
+          <div class="myResume-plate-title-box">
+            <span class="info-t">工作经历</span>
+           <img src="../../../assets/image/Frame_7.png" alt="" class="info-icon-img"/>
+          </div>
+          <div class="myResume-plate-list">
+            <ul class="plate-list-ul">
+              <li>
+                <div class="li-title-box">
+                  <div class="li-name">
+                    <span class="li-name-1">宁波荣胜网络科技有限公司</span>
+                    <span class="li-name-2">2014-2015</span>
+                  </div>
+                  <div class="info-set">
+                    <span>删除</span>
+                    <span>/</span>
+                    <span>编辑</span>
+                  </div>
+                </div>
+                <div class="items-sub-box">设计总监</div>
+                <div class="items-text-box">
+                  负责规划方案设计的技术指导工作；2、负责公司开发产品总体效果的控制；3、组织落实规划建筑、室内装修的设计方案及施工图；4、负责组织审查建筑专业的图纸并提
+                </div>
+              </li>
+              <li>
+                <div class="li-title-box">
+                  <div class="li-name">
+                    <span class="li-name-1">宁波荣胜网络科技有限公司</span>
+                    <span class="li-name-2">2014-2015</span>
+                  </div>
+                  <div class="info-set">
+                    <span>删除</span>
+                    <span>/</span>
+                    <span>编辑</span>
+                  </div>
+                </div>
+                <div class="items-sub-box">设计总监</div>
+                <div class="items-text-box">
+                  负责规划方案设计的技术指导工作；2、负责公司开发产品总体效果的控制；3、组织落实规划建筑、室内装修的设计方案及施工图；4、负责组织审查建筑专业的图纸并提
+                </div>
+              </li>
+            </ul>
+
+          </div>
+        </div>
+        <!-- 工作经历 结束 -->
+        <!-- 项目经历 开始 -->
+        <div class="projectExperience-box myResume-plate">
+          <div class="myResume-plate-title-box">
+            <span class="info-t">项目经历</span>
+           <img src="../../../assets/image/Frame_7.png" alt="" class="info-icon-img"/>
+          </div>
+          <div class="myResume-plate-list">
+            <ul class="plate-list-ul">
+              <li>
+                <div class="li-title-box">
+                  <div class="li-name">
+                    <span class="li-name-1">鄞州非遗馆</span>
+                    <span class="li-name-2">2014-2015</span>
+                  </div>
+                  <div class="info-set">
+                    <span>删除</span>
+                    <span>/</span>
+                    <span>编辑</span>
+                  </div>
+                </div>
+                <div class="items-sub-box">宁波彩壳广告有限公司·设计师</div>
+                <div class="items-text-box">
+                  负责非遗馆所有平面物料。展厅策划
+                </div>
+              </li>
+              <li>
+                <div class="li-title-box">
+                  <div class="li-name">
+                    <span class="li-name-1">鄞州非遗馆</span>
+                    <span class="li-name-2">2014-2015</span>
+                  </div>
+                  <div class="info-set">
+                    <span>删除</span>
+                    <span>/</span>
+                    <span>编辑</span>
+                  </div>
+                </div>
+                <div class="items-sub-box">宁波彩壳广告有限公司·设计师</div>
+                <div class="items-text-box">
+                  负责非遗馆所有平面物料。展厅策划
+                </div>
+              </li>
+            </ul>
+
+          </div>
+        </div>
+        <!-- 项目经历 结束 -->
+        <!-- 教育经历 开始 -->
+        <div class="educationExperience-box myResume-plate">
+          <div class="myResume-plate-title-box">
+            <span class="info-t">教育经历</span>
+           <img src="../../../assets/image/Frame_7.png" alt="" class="info-icon-img"/>
+          </div>
+          <div class="myResume-plate-list">
+            <ul class="plate-list-ul">
+              <li>
+                <div class="li-title-box">
+                  <div class="li-name">
+                    <span class="li-name-1">金职院</span>
+                    <span class="li-name-2">2014-2015</span>
+                  </div>
+                  <div class="info-set">
+                    <span>编辑</span>
+                  </div>
+                </div>
+                <div class="items-sub-box">统招</div>
+                <div class="items-text-box"> 大专|工业设计</div>
+              </li>
+
+            </ul>
+
+          </div>
+        </div>
+        <!-- 教育经历 结束 -->
 
       </div>
+      <!-- 右侧模块 开始 -->
       <div class="info-right-box">
-
+        <div class="right-box up-attachments-box">
+          <div class="up-att-t">
+            <span class="span-1">上传附件简历</span>
+            <span class="span-2">去上传<i class="el-icon-arrow-right" style="margin-left:2px;"></i></span>
+          </div>
+          <div class="up-box-Instructions">在附件中展示更多优势与特长</div>
+        </div>
       </div>
+      <!-- 右侧模块 结束 -->
     </div>
 
 
@@ -135,7 +284,7 @@ export default {
       background: #fff;
       padding: 24px 30px;
       margin-bottom: 16px;
-      .jobExpectation-title-box{
+      .myResume-plate-title-box{
         display: flex;
         align-items: center;
         justify-content: space-between;
@@ -151,9 +300,12 @@ export default {
           cursor: pointer;
         }
       }
+    }
+    // =====  求职期望   ↓=====
+    .jobExpectation-box{
       .myResume-plate-list{
         width: 100%;
-        margin-top: 20px;
+        margin-top: 10px;
         .plate-list-ul{
           width: 100%;
           li{
@@ -178,23 +330,187 @@ export default {
             &:hover{
               background: #F7F8FA;
             }
+            &:hover .info-set{
+              display: block;
+            }
           }
           
         }
       }
-      
     }
- 
+    // =====  求职期望   ↑=====
+    // =====  工作经历/项目经历   ↓=====
+    .workExperience-box,.projectExperience-box{
+      .myResume-plate-list{
+        width: 100%;
+        margin-top: 10px;
+        .plate-list-ul{
+          width: 100%;
+          li{
+            padding: 12px 10px 12px 20px;
+            font-size: 14px;
+            text-align: left;
+            &:hover{
+              background: #F7F8FA;
+            }
+            &:hover .info-set{
+              display: block;
+            }
+            .li-title-box{
+              display: flex;
+              align-items: center;
+              justify-content: space-between;
+              .li-name{
+                flex: 1;
+                text-align: left;
+                color: $g_textColor;
+                .li-name-1{
+                  font-size: 15px;
+                  font-weight: bold;
+                }
+                .li-name-2{
+                  color: #86909C;
+                  padding-left: 30px;
+                }
+              }
+
+            }
+            .items-sub-box{
+              font-size: 14px;
+              font-weight: 400;
+              color: #4E5969;
+              line-height: 22px;
+            }
+            .items-text-box{
+              margin-top: 0.8rem;
+              color: $g_textColor;
+              font-size: 14px;
+              font-weight: 400;
+              line-height: 22px;
+            }
+            
+          }
+          
+        }
+      }
+    }
+
+    // =====  工作经历/项目经历  ↑=====
+
+    // =====  教育经历   ↓=====
+    .educationExperience-box{
+      .myResume-plate-list{
+        width: 100%;
+        margin-top: 10px;
+        .plate-list-ul{
+          width: 100%;
+          li{
+            padding: 12px 10px 12px 20px;
+            font-size: 14px;
+            text-align: left;
+            &:hover{
+              background: #F7F8FA;
+            }
+            &:hover .info-set{
+              display: block;
+            }
+            .li-title-box{
+              display: flex;
+              align-items: center;
+              justify-content: space-between;
+              .li-name{
+                flex: 1;
+                text-align: left;
+                color: $g_textColor;
+                .li-name-1{
+                  font-size: 14px;
+                  font-weight: bold;
+                }
+                .li-name-2{
+                  color: #86909C;
+                  padding-left: 20px;
+                }
+              }
+
+            }
+            .items-sub-box{
+              width: 60px;
+              height: 22px;
+              text-align: center;
+              background: #FFF7E6;
+              border-radius: 2px;
+              opacity: 1;
+              border: 1px solid #FFD591;
+              font-size: 12px;
+              font-weight: 400;
+              color: #FA8C16;
+              line-height: 20px;
+              margin-top: 4px;
+            }
+            .items-text-box{
+              margin-top: 0.8rem;
+              color: $g_textColor;
+              font-size: 14px;
+              font-weight: 400;
+              line-height: 22px;
+            }
+            
+          }
+          
+        }
+      }
+    }
+
+    // =====  教育经历  ↑=====
+
   }
   .info-set{
-    font-size: 0.7rem;
+    font-size: 14px;
     font-weight: 400;
     color: $g_color;
     cursor: pointer;
+    display: none;
+    span{
+      padding-left: 6px;
+    }
   }
+
   .info-right-box{
     width: 406px;
-    padding-left: 16px;
+    padding-left: 0.8rem;
+    .right-box{
+      background: #FFFFFF;
+      border-radius: 6px ;
+      opacity: 1;
+      padding: 24px 24px 20px 24px;
+    }
+    .up-attachments-box{
+      .up-att-t{
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        .span-1{
+          font-size: 15px;
+          font-weight: bold;
+          color: $g_textColor;
+          line-height: 24px;
+        }
+        .span-2{
+          font-size: 14px;
+          font-weight: 400;
+          color: $g_color;
+          line-height: 22px;
+        }
+      }
+      .up-box-Instructions{
+        font-size: 14px;
+        font-weight: 400;
+        color: #86909C;
+        line-height: 22px;
+        margin-top: 8px;
+        text-align: left;
+      }
+    }
   }
  }
 
