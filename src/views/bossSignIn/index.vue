ad47abc267dca0cd4473f42195db2615bb69a332
gaoxizhong <296203386@qq.com>
2023-09-22 18:21:29 +0800

    优化

---
+++ src/views/bossSignIn/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="boss-container">
+    <!-- 我的招聘数据页 -->
+    <div>boss注册</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
